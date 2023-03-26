1086ce6f23b6b13e6ed60d75d60601b881eb6f1d
hanum <rntls123@naver.com>
2023-03-26 22:16:39 +0900

    [add] filter atom

--- src/atoms.ts
+++ src/atoms.ts
@@ -4,3 +4,7 @@ export const isDefaultThemeAtom = atom<boolean>({
   key: "isDefaultTheme",
   default: true,
 });
+export const filteredAuthor = atom<string>({
+  key: "filteredAuthor",
+  default: "",
+});
