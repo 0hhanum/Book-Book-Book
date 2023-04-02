2d4ac5b756fc92a83b85e6ce019d7fcbf588f817
hanum <rntls123@naver.com>
2023-04-02 19:31:57 +0900

    change to meshStandardMaterial - much better

--- src/components/ThreeJS/ThreeBook.tsx
+++ src/components/ThreeJS/ThreeBook.tsx
@@ -1,7 +1,7 @@
 import { OrbitControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import React, { useEffect, useRef } from "react";
-import { BoxGeometry, Color, Float32BufferAttribute, Mesh } from "three";
+import { Mesh } from "three";
 import ThreeLayout from "./ThreeLayout";
 
 interface IThreeBook {
@@ -12,29 +12,30 @@ interface IBookObject {
 }
 function BookObject(props: IBookObject) {
   const meshRef = useRef<Mesh>(null);
-  // useFrame((state, delta) => {
-  //   if (meshRef.current) {
-  //     meshRef.current.rotation.y += delta * 0.5;
-  //   }
-  // });
+  useFrame((_, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y -= delta * 0.5;
+      meshRef.current.rotation.z += delta * 0.25;
+    }
+  });
 
   return (
     <mesh {...props} scale={1} ref={meshRef}>
       <boxGeometry args={[0.6, 0.8, 0.15]} attach="geometry" />
-      <meshBasicMaterial color="white" attach="material-0" />
-      <meshBasicMaterial color="green" attach="material-1" />
-      <meshBasicMaterial color="white" attach="material-2" />
-      <meshBasicMaterial color="white" attach="material-3" />
-      <meshBasicMaterial color="green" attach="material-4" />
-      <meshBasicMaterial color="green" attach="material-5" />
+      <meshStandardMaterial color="white" attach="material-0" />
+      <meshStandardMaterial color="green" attach="material-1" />
+      <meshStandardMaterial color="white" attach="material-2" />
+      <meshStandardMaterial color="white" attach="material-3" />
+      <meshStandardMaterial color="green" attach="material-4" />
+      <meshStandardMaterial color="green" attach="material-5" />
     </mesh>
   );
 }
 const ThreeBook = ({ children }: IThreeBook) => {
   return (
     <ThreeLayout>
-      <ambientLight intensity={1.5} />
-      <spotLight position={[5, 5, 10]} angle={0.15} penumbra={1} />
+      <ambientLight intensity={0.8} />
+      <spotLight position={[-5, 10, 10]} angle={0.5} penumbra={0.5} />
       <BookObject position={[0, 0, 0]} />
       <OrbitControls />
     </ThreeLayout>
