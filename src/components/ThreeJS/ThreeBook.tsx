ea6ba7220085a82ee3bdad24ac7372c863a9c453
hanum <rntls123@naver.com>
2023-04-02 19:59:16 +0900

    dispose all objects to avoid memory leaking

--- src/components/ThreeJS/ThreeBook.tsx
+++ src/components/ThreeJS/ThreeBook.tsx
@@ -1,7 +1,7 @@
 import { OrbitControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import React, { useEffect, useRef } from "react";
-import { Mesh } from "three";
+import { BoxGeometry, Mesh, MeshStandardMaterial } from "three";
 import ThreeLayout from "./ThreeLayout";
 
 interface IThreeBook {
@@ -12,6 +12,19 @@ interface IBookObject {
 }
 function BookObject(props: IBookObject) {
   const meshRef = useRef<Mesh>(null);
+  const boxRef = useRef<BoxGeometry>(null);
+  const materialRefs = useRef<MeshStandardMaterial[]>([]);
+  // dispose all objects to avoid memory leaking
+  useEffect(() => {
+    return () => {
+      boxRef.current?.dispose();
+      materialRefs.current.forEach((materialRef) => {
+        if (materialRef) {
+          materialRef.dispose();
+        }
+      });
+    };
+  }, []);
   useFrame((_, delta) => {
     if (meshRef.current) {
       meshRef.current.rotation.y -= delta * 0.5;
@@ -22,14 +35,37 @@ function BookObject(props: IBookObject) {
   return (
     <mesh {...props} scale={1} ref={meshRef}>
       <spotLight position={[-5, 10, 10]} angle={0.5} penumbra={0.5} />
-
-      <boxGeometry args={[0.6, 0.8, 0.15]} attach="geometry" />
-      <meshStandardMaterial color="white" attach="material-0" />
-      <meshStandardMaterial color="green" attach="material-1" />
-      <meshStandardMaterial color="white" attach="material-2" />
-      <meshStandardMaterial color="white" attach="material-3" />
-      <meshStandardMaterial color="green" attach="material-4" />
-      <meshStandardMaterial color="green" attach="material-5" />
+      <boxGeometry ref={boxRef} args={[0.6, 0.8, 0.15]} attach="geometry" />
+      <meshStandardMaterial
+        color="white"
+        attach="material-0"
+        ref={(ref) => (ref ? materialRefs.current.push(ref!) : null)}
+      />
+      <meshStandardMaterial
+        color="green"
+        attach="material-1"
+        ref={(ref) => (ref ? materialRefs.current.push(ref!) : null)}
+      />
+      <meshStandardMaterial
+        color="white"
+        attach="material-2"
+        ref={(ref) => (ref ? materialRefs.current.push(ref!) : null)}
+      />
+      <meshStandardMaterial
+        color="white"
+        attach="material-3"
+        ref={(ref) => (ref ? materialRefs.current.push(ref!) : null)}
+      />
+      <meshStandardMaterial
+        color="green"
+        attach="material-4"
+        ref={(ref) => (ref ? materialRefs.current.push(ref!) : null)}
+      />
+      <meshStandardMaterial
+        color="green"
+        attach="material-5"
+        ref={(ref) => (ref ? materialRefs.current.push(ref!) : null)}
+      />
     </mesh>
   );
 }
