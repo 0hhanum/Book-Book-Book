3db0c6f600ad31e00927daa0c34bd4c63d1d6775
hanum <rntls123@naver.com>
2023-04-02 19:04:43 +0900

    make book color but it looks like doesnt affected by light...

--- src/components/ThreeJS/ThreeBook.tsx
+++ src/components/ThreeJS/ThreeBook.tsx
@@ -11,41 +11,29 @@ interface IBookObject {
   position: [number, number, number];
 }
 function BookObject(props: IBookObject) {
-  const boxRef = useRef<BoxGeometry>(null);
   const meshRef = useRef<Mesh>(null);
-  useEffect(() => {
-    if (boxRef.current) {
-      const geometry = boxRef.current.toNonIndexed();
-      const positionAttribute = geometry.getAttribute("position");
-      const colors = [];
-      const color = new Color();
-
-      for (let i = 0; i < positionAttribute.count; i += 3) {
-        color.set(Math.random() * 0xffffff);
-        colors.push(color.r, color.g, color.b);
-        colors.push(color.r, color.g, color.b);
-        colors.push(color.r, color.g, color.b);
-      }
-      geometry.setAttribute("color", new Float32BufferAttribute(colors, 3));
-    }
-  }, [boxRef]);
-  useFrame((state, delta) => {
-    if (meshRef.current) {
-      meshRef.current.rotation.y += delta;
-    }
-  });
+  // useFrame((state, delta) => {
+  //   if (meshRef.current) {
+  //     meshRef.current.rotation.y += delta * 0.5;
+  //   }
+  // });
 
   return (
     <mesh {...props} scale={1} ref={meshRef}>
-      <boxGeometry ref={boxRef} args={[0.6, 0.8, 0.15]} />
-      <meshNormalMaterial vertexColors={true} />
+      <boxGeometry args={[0.6, 0.8, 0.15]} attach="geometry" />
+      <meshBasicMaterial color="white" attach="material-0" />
+      <meshBasicMaterial color="green" attach="material-1" />
+      <meshBasicMaterial color="white" attach="material-2" />
+      <meshBasicMaterial color="white" attach="material-3" />
+      <meshBasicMaterial color="green" attach="material-4" />
+      <meshBasicMaterial color="green" attach="material-5" />
     </mesh>
   );
 }
 const ThreeBook = ({ children }: IThreeBook) => {
   return (
     <ThreeLayout>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={1.5} />
       <spotLight position={[5, 5, 10]} angle={0.15} penumbra={1} />
       <BookObject position={[0, 0, 0]} />
       <OrbitControls />
