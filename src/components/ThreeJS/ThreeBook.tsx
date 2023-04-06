f0a1069c8e7e4b347086b5c9dfb6910577c17a49
hanum <rntls123@naver.com>
2023-04-07 00:25:14 +0900

    test load external texture. it works well

--- src/components/ThreeJS/ThreeBook.tsx
+++ src/components/ThreeJS/ThreeBook.tsx
@@ -3,44 +3,59 @@ import {
   Stars,
   useGLTF,
   useAnimations,
+  useTexture,
 } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import React, { useEffect, useRef, useState } from "react";
-import { Group, Vector3 } from "three";
+import {
+  Group,
+  Mesh,
+  MeshStandardMaterial,
+  TextureLoader,
+  Vector3,
+} from "three";
 import ThreeLayout from "./ThreeLayout";
 
 interface IThreeBook {
   children: any;
 }
 interface IBookObject {
-  position?: [number, number, number];
+  position: [number, number, number];
+  rotation: [number, number, number];
 }
 
 const BookObject = React.memo((props: IBookObject) => {
   const groupRef = useRef<Group>(null);
-  // const bookSceneRef = useRef<Mesh>(null);
   const { scene, animations } = useGLTF("/bookModel/scene.gltf");
   const { actions, ref: bookSceneRef } = useAnimations(animations, groupRef);
-  const { camera } = useThree();
   const [isZoomedIn, setIsZoomedIn] = useState(false);
+  const { camera } = useThree();
 
   useEffect(() => {
-    camera?.position.set(0, 0, 15);
+    camera?.position.set(0, 0, 20);
   }, [camera]);
   useEffect(() => {
     // actions["Demo"]?.play();
   }, [actions]);
   useEffect(() => {
     const mesh = bookSceneRef.current?.getObjectByName("Book_0") as Mesh;
+    const bookCoverMaterial = new MeshStandardMaterial({ color: "wheat" });
+    const textureLoader = new TextureLoader();
+    textureLoader.load(
+      "/bookModel/textures/Base_metallicRoughness.png",
+      (texture) => {
+        bookCoverMaterial.map = texture;
+        mesh.material = bookCoverMaterial;
+      }
+    );
   }, [bookSceneRef]);
-  useFrame((state, delta) => {
+  useFrame((_, delta) => {
     if (!isZoomedIn) {
       // Zoom in
-      const targetPos = new Vector3(0, 0, 7);
-      const curPos = camera.position;
-      camera.position.lerp(targetPos, 0.05);
-      camera.lookAt(0, 0, 0);
-      const dist = curPos.distanceTo(targetPos);
+      const targetPosition = new Vector3(0, 0, 7);
+      const currentPosition = camera.position;
+      camera.position.lerp(targetPosition, 0.05);
+      const dist = currentPosition.distanceTo(targetPosition);
       if (dist < 0.1) {
         setIsZoomedIn(true);
       }
@@ -56,12 +71,7 @@ const BookObject = React.memo((props: IBookObject) => {
   return (
     <group ref={groupRef}>
       <spotLight position={[-5, 10, 10]} angle={0.5} penumbra={0.5} />
-      <primitive
-        object={scene}
-        {...props}
-        ref={bookSceneRef}
-        rotation={[Math.PI / 2, 0, -Math.PI / 2]}
-      />
+      <primitive object={scene} {...props} ref={bookSceneRef} />
       <Stars />
     </group>
   );
@@ -70,7 +80,10 @@ const ThreeBook = ({ children }: IThreeBook) => {
   return (
     <ThreeLayout>
       <ambientLight intensity={0.8} />
-      <BookObject position={[-1, 0, 0]} />
+      <BookObject
+        position={[-1, 0, 0]}
+        rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+      />
       <OrbitControls />
     </ThreeLayout>
   );
