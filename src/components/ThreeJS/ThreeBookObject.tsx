caebbd4e51b406915df1a9733116ee2139616c35
hanum <rntls123@naver.com>
2023-04-11 22:56:33 +0900

    make animation clear and diving to the book effect

--- src/components/ThreeJS/ThreeBookObject.tsx
+++ src/components/ThreeJS/ThreeBookObject.tsx
@@ -27,12 +27,14 @@ const BookObject = React.memo(({ book, ...props }: IBookObject) => {
   const [isZoomedIn, setIsZoomedIn] = useState(false);
   const [isLoadingTexture, setIsLoadingTexture] = useState(false);
   const [clickAnimation, setClickAnimation] = useState([false, false]);
-  const [animationToggle, setAnimationToggle] = useState(false);
+  const [openAnimation, setOpenAnimation] = useState(false);
   const setIsHover = useSetRecoilState(cursorStyleAtom);
   const { camera } = useThree();
+  // set initial camera position
   useEffect(() => {
     camera?.position.set(0, 0, 20);
   }, [camera]);
+  // load and set book cover texture
   useEffect(() => {
     const bookMesh = bookSceneRef.current?.getObjectByName("Book_0") as Mesh;
     const bookMaterial = bookMesh.material as MeshStandardMaterial;
@@ -45,9 +47,10 @@ const BookObject = React.memo(({ book, ...props }: IBookObject) => {
       callback: () => setIsLoadingTexture(true),
     });
   }, [bookSceneRef]);
+
   useFrame((_, delta) => {
     if (!isZoomedIn && isLoadingTexture) {
-      // Zoom in
+      // zoom in
       const targetCameraPosition = new Vector3(0, 0, 7);
       const currentPosition = camera.position;
       camera.position.lerp(targetCameraPosition, 0.05);
@@ -55,61 +58,60 @@ const BookObject = React.memo(({ book, ...props }: IBookObject) => {
       if (dist < 0.1) {
         setIsZoomedIn(true);
       }
-    } else {
-      if (bookSceneRef.current) {
-        // book opening animation
-        if (clickAnimation[0]) {
-          if (!animationToggle) {
-            // before zoom
-            const targetCameraPosition = new Vector3(0, 0, 10);
-            camera.position.lerp(targetCameraPosition, 0.04);
-            const currentPosition = camera.position;
-            const targetRotation = new Quaternion();
-            targetRotation.setFromEuler(new Euler(0, -Math.PI / 2, 0));
-            bookSceneRef.current.quaternion.slerp(targetRotation, 0.05);
-            const cameraDistance =
-              currentPosition.distanceTo(targetCameraPosition);
-            const rotationDifference =
-              bookSceneRef.current.quaternion.angleTo(targetRotation);
-            if (cameraDistance < 0.5 && rotationDifference === 0) {
-              setAnimationToggle(true);
-            }
-          } else if (clickAnimation[1]) {
-            // after zoom
-            const targetCameraPosition = new Vector3(5, 0, 5);
-            camera.position.lerp(targetCameraPosition, 0.04);
+    } else if (bookSceneRef.current) {
+      const bookRef = bookSceneRef.current;
+      if (!clickAnimation[0]) {
+        // Rotate the book
+        bookRef.rotation.y += delta * 0.4;
+        bookRef.rotation.z += delta * 0.225;
+      } else {
+        // clickAnimation[0]: set camera and book for fixed position
+        // clickAnimation[1]: dive to book
+        if (!clickAnimation[1]) {
+          // make distance from the book (camera)
+          const targetCameraPosition = new Vector3(0, 0, 10);
+          camera.position.lerp(targetCameraPosition, 0.04);
+          const currentPosition = camera.position;
+          // rotate book to see forward (book)
+          const targetRotation = new Quaternion();
+          targetRotation.setFromEuler(new Euler(0, -Math.PI / 2, 0));
+          bookRef!.quaternion.slerp(targetRotation, 0.05);
+          const cameraDistance =
+            currentPosition.distanceTo(targetCameraPosition);
+          const rotationDifference =
+            bookRef!.quaternion.angleTo(targetRotation);
+          // if position is set, start next animation(open the book)
+          if (cameraDistance < 0.5 && rotationDifference < 1) {
+            // if use same state, play() of 3d model called every time
+            setOpenAnimation(true);
           }
-        } else {
-          // Rotate the book
-          bookSceneRef.current.rotation.y += delta * 0.4;
-          bookSceneRef.current.rotation.z += delta * 0.225;
+        } else if (clickAnimation[1]) {
+          // after zoom
+          const targetCameraPosition = new Vector3(0, 0, 1);
+          camera.position.lerp(targetCameraPosition, 0.04);
         }
       }
     }
   });
+
   useEffect(() => {
-    if (animationToggle) {
-      actions["Demo"]?.play();
+    if (openAnimation) {
+      actions["Demo"]!.play();
       setTimeout(() => {
-        actions["Demo"]?.stop();
+        actions["Demo"]!.paused = true;
+        // dive in to book
         setClickAnimation([true, true]);
-      }, 2000);
+      }, 1500);
     }
-  }, [animationToggle]);
+  }, [openAnimation]);
   const onClick = () => {
     if (isZoomedIn) {
-      // bookSceneRef.current!.rotation.y = -Math.PI / 2;
-      // bookSceneRef.current!.rotation.z = 0;
-      // const targetRotation = new Quaternion();
-      // targetRotation.setFromEuler(new Euler(-Math.PI / 2, 0, 0));
-      // setInterval(() => {
-      //   bookSceneRef.current!.quaternion.slerp(targetRotation, 0.05);
-      // });
       setClickAnimation([true, false]);
     }
   };
   return (
     <group ref={groupRef}>
+      {/* light for opposite position */}
       <spotLight position={[0, 10, 20]} angle={0.4} penumbra={0.2} />
       <spotLight position={[0, 10, -20]} angle={0.4} penumbra={0.2} />
       <primitive
