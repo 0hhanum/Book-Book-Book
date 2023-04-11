df39c6e590a6ce7f2409555fc775b66a9c840f2b
hanum <rntls123@naver.com>
2023-04-12 00:54:51 +0900

    add content type filter (contentful)

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,6 +14,7 @@ const config: GatsbyConfig = {
       options: {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         spaceId: "drmlgrx4rnh5",
+        contentTypeFilter: (contentType: any) => contentType.sys.id === "books",
       },
     },
     "gatsby-plugin-image",
