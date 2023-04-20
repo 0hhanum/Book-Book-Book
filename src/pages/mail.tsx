205f2607133e00bd4fbf34e7ae2f7b8eb619e5a6
hanum <rntls123@naver.com>
2023-04-21 01:13:13 +0900

    mailform

--- src/pages/mail.tsx
+++ src/pages/mail.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import Helmet from "../components/Helmet";
 import styled from "styled-components";
 
@@ -15,7 +15,7 @@ const ContentSection = styled.section`
   &:first-child {
     border-right: ${(props) => `1px solid ${props.theme.normalColor}`};
     display: flex;
-    align-items: flex-end;
+    align-items: center;
     h1 {
       font-size: 128px;
       margin-bottom: 24px;
@@ -24,10 +24,21 @@ const ContentSection = styled.section`
   &:last-child {
     h1 {
       font-size: 64px;
-      letter-spacing: 8px;
+      letter-spacing: 12px;
+    }
+    h1:last-child {
+      border-bottom: ${(props) => `1px solid ${props.theme.normalColor}`};
     }
   }
 `;
+const FormContainer = styled.div``;
+const Form = styled.form``;
+const FormButton = styled.button``;
+const Title = styled.input``;
+const Content = styled.textarea``;
+const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+};
 const Mail = () => {
   return (
     <Container>
@@ -38,6 +49,13 @@ const Mail = () => {
         <ContentSection>
           <h1>rntls123@</h1>
           <h1>naver.com</h1>
+          <FormContainer>
+            <Form onSubmit={onSubmit}>
+              <FormButton>Send</FormButton>
+              <Title />
+              <Content />
+            </Form>
+          </FormContainer>
         </ContentSection>
       </ContentLayout>
     </Container>
