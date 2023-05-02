156796919fcd3c96c8d4cf24b37e977fc7740d99
hanum <rntls123@naver.com>
2023-05-03 00:19:14 +0900

    [bugfix] clear cursor-pointer when unmount threeJS dialog

--- src/components/ThreeJS/ThreeCanvas.tsx
+++ src/components/ThreeJS/ThreeCanvas.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
+import { cursorStyleAtom } from "../../atoms";
+import { useSetRecoilState } from "recoil";
 
 interface IThreeCanvas {
   children: any;
 }
 
 const ThreeCanvas = ({ children }: IThreeCanvas) => {
+  const setIsHover = useSetRecoilState(cursorStyleAtom);
+  // clear pointer cursor when unmounted
+  useEffect(() => {
+    return () => {
+      setIsHover(false);
+    };
+  }, []);
+
   return (
     <Canvas
       gl={{ antialias: false, alpha: false }}
