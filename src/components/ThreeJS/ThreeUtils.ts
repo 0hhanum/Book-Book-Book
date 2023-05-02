50ed2fd5733fce335307eb11c1507261e5dda69b
hanum <rntls123@naver.com>
2023-05-03 00:15:40 +0900

    error handle for load threejs book texture

--- src/components/ThreeJS/ThreeUtils.ts
+++ src/components/ThreeJS/ThreeUtils.ts
@@ -19,28 +19,33 @@ export const loadTexture = ({
   callback,
 }: ILoadTexture) => {
   const textureLoader = new TextureLoader();
-  textureLoader.load(bookCoverSrc, (texture) => {
-    // to create high quality texture
-    texture.generateMipmaps = true;
-    texture.repeat.set(1, 1);
-    texture.offset.set(0, 0);
-    texture.center.set(0, 0);
-    texture.rotation = 0;
-    texture.minFilter = LinearFilter;
-    texture.needsUpdate = true;
-    texture.anisotropy = 1;
-    texture.flipY = false;
-    texture.wrapS = RepeatWrapping;
-    texture.wrapT = RepeatWrapping;
-    texture.format = RGBAFormat;
-    texture.type = UnsignedByteType;
-    texture.encoding = sRGBEncoding;
-    texture.minFilter = LinearFilter;
-    texture.magFilter = LinearFilter;
-    // change book cover
-    material.map = texture;
+  try {
+    textureLoader.load(bookCoverSrc, (texture) => {
+      // to create high quality texture
+      texture.generateMipmaps = true;
+      texture.repeat.set(1, 1);
+      texture.offset.set(0, 0);
+      texture.center.set(0, 0);
+      texture.rotation = 0;
+      texture.minFilter = LinearFilter;
+      texture.needsUpdate = true;
+      texture.anisotropy = 1;
+      texture.flipY = false;
+      texture.wrapS = RepeatWrapping;
+      texture.wrapT = RepeatWrapping;
+      texture.format = RGBAFormat;
+      texture.type = UnsignedByteType;
+      texture.encoding = sRGBEncoding;
+      texture.minFilter = LinearFilter;
+      texture.magFilter = LinearFilter;
+      // change book cover
+      material.map = texture;
+      callback();
+    });
+  } catch (e) {
+    console.log(e);
     callback();
-  });
+  }
 };
 
 export const convertToKebabCase = (str: string): string => {
