9988e6a45ae91ceceb8beb53d2b76327ecfb8abc
hanum <rntls123@naver.com>
2023-05-08 01:29:55 +0900

    toggle mute and camera in video component

--- src/components/RTCs/VideoComponent.tsx
+++ src/components/RTCs/VideoComponent.tsx
@@ -1,16 +1,63 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 interface IVideo {
   stream: MediaStream;
   isOwnVideo: boolean;
 }
-const Container = styled.div``;
-const Video = styled.video`
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 640px;
+  height: 480px;
+  position: relative;
+`;
+const BtnContainer = styled.div`
+  display: flex;
+  bottom: 0;
+  position: absolute;
+`;
+const Btn = styled.div<{ isActive: boolean }>`
+  width: 110px;
+  height: 50px;
+  border-radius: 50%;
   border: ${(props) => `1px solid ${props.theme.fontColor}`};
+  background-color: ${(props) =>
+    props.isActive ? props.theme.headerColor : props.theme.backgroundColor};
+  color: ${(props) =>
+    props.isActive ? props.theme.backgroundColor : props.theme.fontColor};
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:first-child {
+    margin-right: 20px;
+  }
+`;
+const Video = styled.video`
+  width: 100%;
+  height: 100%;
 `;
 const VideoComponent = (props: IVideo) => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [isMuted, setIsMuted] = useState(false);
+  const [isCameraOff, setIsCameraOff] = useState(false);
+  const toggleMute = () => {
+    setIsMuted((current) => {
+      props.stream.getAudioTracks().forEach((track) => {
+        track.enabled = current;
+      });
+      return !current;
+    });
+  };
+  const toggleCamera = () => {
+    setIsCameraOff((current) => {
+      props.stream.getVideoTracks().forEach((track) => {
+        track.enabled = current;
+      });
+      return !current;
+    });
+  };
   useEffect(() => {
     if (!videoRef.current) return;
     videoRef.current.srcObject = props.stream;
@@ -18,6 +65,14 @@ const VideoComponent = (props: IVideo) => {
   return (
     <Container>
       <Video ref={videoRef} autoPlay playsInline muted={props.isOwnVideo} />
+      <BtnContainer>
+        <Btn isActive={isMuted} onClick={toggleMute}>
+          {isMuted ? "Unmute" : "Mute"}
+        </Btn>
+        <Btn isActive={isCameraOff} onClick={toggleCamera}>
+          {isCameraOff ? "Camera On" : "Camera Off"}
+        </Btn>
+      </BtnContainer>
     </Container>
   );
 };
