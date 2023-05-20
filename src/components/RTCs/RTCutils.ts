206f81caf8a147a67afd03766f1af804b928424a
hanum <rntls123@naver.com>
2023-05-20 21:10:29 +0900

    Fix(RTCUtils): Fix SSR build issue to use navigator object only in the browser environment

--- src/components/RTCs/RTCutils.ts
+++ src/components/RTCs/RTCutils.ts
@@ -1,4 +1,6 @@
 export const setStream = async () => {
+  if (typeof navigator === "undefined") return;
+  // This code executed only in the browser environment
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: {
