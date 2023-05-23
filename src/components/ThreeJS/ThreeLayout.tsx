cb3bfc78854ae599f20e4a78a9e5075e1abb74ae
hanum <rntls123@naver.com>
2023-05-23 23:55:37 +0900

    Revert "Refactor(ThreeLayout): Suspense is not necessary in this case"
    This reverts commit 1ed8766d3f68cedea3855b7d659d8217e8e5fd14.

--- src/components/ThreeJS/ThreeLayout.tsx
+++ src/components/ThreeJS/ThreeLayout.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-import ThreeCanvas from "./ThreeCanvas";
+import React, { Suspense, lazy } from "react";
 
 interface IThreeLayout {
   children: any;
 }
 
+const ThreeCanvas = lazy(() => import("./ThreeCanvas"));
+
 const ThreeLayout = ({ children }: IThreeLayout) => {
-  return <ThreeCanvas>{children}</ThreeCanvas>;
+  return (
+    <Suspense>
+      <ThreeCanvas>{children}</ThreeCanvas>
+    </Suspense>
+  );
 };
 
 export default ThreeLayout;
