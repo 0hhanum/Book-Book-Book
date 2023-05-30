a3d0e1f296b9b10d595cf430ae9e587226979a72
hanum <rntls123@naver.com>
2023-05-31 00:08:39 +0900

    Revert "File(/Animations): Make animtion component directory"
    This reverts commit a31a3f1313c8885476cd5a1bf970274ef3f74dc3.

---
+++ src/components/Common/ScrollAnimationComponent.tsx
@@ -0,0 +1,10 @@
+import { motion } from "framer-motion";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled(motion.div)``;
+const ScrollAnimationComponent = () => {
+  return <Container></Container>;
+};
+
+export default ScrollAnimationComponent;
