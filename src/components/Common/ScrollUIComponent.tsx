7c762f17f907d8d437ffe836a814f4184c521205
hanum <rntls123@naver.com>
2023-05-30 21:39:01 +0900

    Feat(ScrollUIComponent): Add ScrollUIComponent

--- src/components/Common/ScrollUIComponent.tsx
+++ src/components/Common/ScrollUIComponent.tsx
@@ -2,8 +2,65 @@ import { motion } from "framer-motion";
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled(motion.div)`
+  position: fixed;
+  bottom: 5vh;
+  right: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Mouse = styled.div`
+  height: 50px;
+  width: 30px;
+  border: ${(props) => `2px solid ${props.theme.fontColor}`};
+  border-radius: 30px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 8px;
+`;
+const Wheel = styled.div`
+  width: 2px;
+  height: 10px;
+  position: absolute;
+  top: 10px;
+  border-radius: 5px;
+  background-color: ${(props) => props.theme.fontColor};
+`;
+const Arrow = styled.div`
+  width: 12px;
+  height: 12px;
+  border-right: ${(props) => `2px solid ${props.theme.fontColor}`};
+  border-bottom: ${(props) => `2px solid ${props.theme.fontColor}`};
+  transform: rotate(45deg);
+`;
+const ScrollComponentVariants = {
+  initial: {
+    translateY: 0,
+  },
+  animate: {
+    translateY: 20,
+    transition: {
+      repeat: Infinity,
+      duration: 1,
+      repeatType: "reverse",
+    },
+  },
+};
 const ScrollUIComponent = () => {
-  return null;
+  return (
+    <Container
+      variants={ScrollComponentVariants}
+      initial="initial"
+      animate="animate"
+    >
+      <Mouse>
+        <Wheel />
+      </Mouse>
+      <Arrow />
+    </Container>
+  );
 };
 
 export default ScrollUIComponent;
--- src/pages/books/dance-dance-dance.tsx
+++ src/pages/books/dance-dance-dance.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Helmet from "../../components/Helmet";
+import styled from "styled-components";
+import ScrollUIComponent from "../../components/Common/ScrollUIComponent";
 
+const Container = styled.div``;
 const DanceDanceDance = () => {
-  return <h1>test</h1>;
+  return (
+    <Container>
+      <ScrollUIComponent />
+    </Container>
+  );
 };
 
 export default DanceDanceDance;
