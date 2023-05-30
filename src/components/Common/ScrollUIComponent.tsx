9d1fab8ce79457319def26097310f969157bb3d2
hanum <rntls123@naver.com>
2023-05-30 21:19:15 +0900

    File(ScrollUIComponent): Add File

---
+++ src/components/Common/ScrollUIComponent.tsx
@@ -0,0 +1,9 @@
+import { motion } from "framer-motion";
+import React from "react";
+import styled from "styled-components";
+
+const ScrollUIComponent = () => {
+  return null;
+};
+
+export default ScrollUIComponent;
