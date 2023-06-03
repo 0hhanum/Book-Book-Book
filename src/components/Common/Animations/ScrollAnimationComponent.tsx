9bd6b6fff2ced2897122140066d21742a193bbbb
hanum <rntls123@naver.com>
2023-06-03 15:06:34 +0900

    File(SlashEffectComponent): Devide slash effect to component

--- src/components/Common/Animations/ScrollAnimationComponent.tsx
+++ src/components/Common/Animations/ScrollAnimationComponent.tsx
@@ -4,13 +4,9 @@ import styled from "styled-components";
 
 export interface IScrollAnimationComponent {
   texts: string[];
-  flashEffect?: boolean;
+  children?: any;
 }
 const INTERSECTION_AMOUNT = 0.3;
-const FLASH_EFFECT_OPACITY_DELAY = 0.5;
-const FLASH_EFFECT_OPACITY_DURATION = 0.5;
-const FLASH_EFFECT_BROKEN_DELAY = 1.5;
-const FLASH_EFFECT_BROKEN_DURATION = 0.1;
 
 const Container = styled(motion.div)`
   height: 225vh;
@@ -25,23 +21,6 @@ const TextContainer = styled(motion.div)`
   flex-direction: column;
   z-index: 2;
 `;
-const FlashEffect = styled(motion.div)`
-  position: fixed;
-  top: ${(props) => `${props.theme.variables.headerHeight}px`};
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: #a70e0e;
-  z-index: 1;
-  opacity: 0;
-`;
-const FlashEffectLeft = styled(FlashEffect)`
-  clip-path: polygon(25% 0, 75% 100%, 0 100%, 0 0);
-`;
-
-const FlashEffectRight = styled(FlashEffect)`
-  clip-path: polygon(100% 0, 100% 100%, 75% 100%, 25% 0);
-`;
 
 const Text = styled.span`
   margin-bottom: 40px;
@@ -51,51 +30,9 @@ const Text = styled.span`
   justify-content: center;
 `;
 
-const FlashEffectVariants = {
-  initial: { x: 0, y: 0 },
-  animate: {
-    opacity: [0, 1],
-    transition: {
-      delay: FLASH_EFFECT_OPACITY_DELAY,
-      duration: FLASH_EFFECT_OPACITY_DURATION,
-      ease: "easeIn",
-      x: {
-        delay: FLASH_EFFECT_BROKEN_DELAY,
-        duration: FLASH_EFFECT_BROKEN_DURATION,
-      },
-      y: {
-        delay: FLASH_EFFECT_BROKEN_DELAY,
-        duration: FLASH_EFFECT_BROKEN_DURATION,
-      },
-      rotate: {
-        delay: FLASH_EFFECT_BROKEN_DELAY,
-        duration: FLASH_EFFECT_BROKEN_DURATION,
-      },
-    },
-  },
-  exit: { display: "none" },
-};
-const FlashEffectLeftVariants = {
-  ...FlashEffectVariants,
-  animate: {
-    ...FlashEffectVariants.animate,
-    x: ["0%", "-5%"],
-    y: ["0%", "-5%"],
-    rotate: ["0deg", "-1deg"],
-  },
-};
-const FlashEffectRightVariants = {
-  ...FlashEffectVariants,
-  animate: {
-    ...FlashEffectVariants.animate,
-    x: ["0%", "5%"],
-    y: ["0%", "5%"],
-    rotate: ["0deg", "2deg"],
-  },
-};
 const ScrollAnimationComponent = ({
   texts,
-  flashEffect = false,
+  children,
 }: IScrollAnimationComponent) => {
   const containerRef = useRef(null);
   const isInView = useInView(containerRef, { amount: INTERSECTION_AMOUNT });
@@ -118,22 +55,7 @@ const ScrollAnimationComponent = ({
                 <Text key={i}>{text}</Text>
               ))}
             </TextContainer>
-            {flashEffect && (
-              <>
-                <FlashEffectLeft
-                  variants={FlashEffectLeftVariants}
-                  initial="initial"
-                  animate="animate"
-                  exit="exit"
-                />
-                <FlashEffectRight
-                  variants={FlashEffectRightVariants}
-                  initial="initial"
-                  animate="animate"
-                  exit="exit"
-                />
-              </>
-            )}
+            {children}
           </>
         )}
       </AnimatePresence>
