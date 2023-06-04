f4912fac785d2ba05fa0b03082d1a4705769ec28
hanum <rntls123@naver.com>
2023-06-05 00:37:13 +0900

    Design(MaseratiInOcean): Set position of Three Layout

--- src/components/Books/DanceDanceDance/MaseratiInOcean.tsx
+++ src/components/Books/DanceDanceDance/MaseratiInOcean.tsx
@@ -3,13 +3,28 @@ import ThreeLayout from "../../ThreeJS/ThreeLayout";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
-const Container = styled(motion.div)``;
+const Container = styled(motion.div)`
+  position: fixed;
+  top: ${(props) => `${props.theme.variables.headerHeight}px`};
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background-color: red;
+`;
 
 const MaseratiInOcean = () => {
   return (
-    <Container initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+    <Container
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 5 }}
+    >
       <ThreeLayout>
         <ambientLight intensity={0.8} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
       </ThreeLayout>
     </Container>
   );
