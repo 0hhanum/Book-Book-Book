fcb1e86b01e45788bae73adeba556d9f8cd43212
hanum <rntls123@naver.com>
2023-06-06 18:33:54 +0900

    Feat(MaseratiInOcean): Limit camera rotation, Distance

--- src/components/Books/DanceDanceDance/MaseratiInOcean.tsx
+++ src/components/Books/DanceDanceDance/MaseratiInOcean.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { memo } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { OrbitControls, Sky } from "@react-three/drei";
+import { OrbitControls, Sky, useGLTF } from "@react-three/drei";
 import ThreeCanvas from "../../ThreeJS/ThreeCanvas";
 import ThreeOcean from "../../ThreeJS/ThreeOcean";
 import { Vector3 } from "three";
 
+const CAMERA_POSITION = new Vector3(100, 10, 0);
+const ANIMATE_DURATION = 0.1;
 const Container = styled(motion.div)`
   position: fixed;
   top: ${(props) => `${props.theme.variables.headerHeight}px`};
@@ -13,22 +15,32 @@ const Container = styled(motion.div)`
   left: 0;
   bottom: 0;
 `;
-
 const MaseratiInOcean = () => {
   return (
     <Container
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 5 }}
+      transition={{ duration: ANIMATE_DURATION }}
     >
-      <ThreeCanvas cameraPosition={new Vector3(100, 5, 0)}>
+      <ThreeCanvas cameraPosition={CAMERA_POSITION}>
         <Sky sunPosition={[0, -1, 0]} turbidity={20} />
-        <ThreeOcean />
-        <boxGeometry />
-        <OrbitControls />
+        <group>
+          <ThreeOcean />
+          <boxGeometry />
+        </group>
+        <OrbitControls
+          minPolarAngle={Math.PI / 6}
+          maxPolarAngle={Math.PI / 2}
+          maxDistance={450}
+        />
       </ThreeCanvas>
     </Container>
   );
 };
 
+const Maserati = memo(() => {
+  const { scene, animations } = useGLTF("/threeModel/bookModel/scene.gltf");
+
+  return null;
+});
 export default MaseratiInOcean;
