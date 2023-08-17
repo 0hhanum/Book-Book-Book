5953303188a38f1e8105e0396f31836f68d77d32
ohhanum <rntls123@naver.com>
2023-08-17 14:25:03 +0900

    Feat(components/utils): Add throttle function

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,33 +1,52 @@
 export const checkImageCached = (src: string) => {
-  if (checkIsSSR()) return;
-  const imgEntries = window.performance.getEntriesByName(src);
-  return imgEntries.length > 0;
+    if (checkIsSSR()) return;
+    const imgEntries = window.performance.getEntriesByName(src);
+    return imgEntries.length > 0;
 };
 
 export const checkIsSSR = (): boolean => {
-  return typeof window === "undefined";
+    return typeof window === "undefined";
 };
 
 export const preloadImage = (src: string, callback?: () => void) => {
-  const virtualImage = new Image();
-  virtualImage.src = src; // it makes load texture concurrently using disc cache
-  if (callback) {
-    virtualImage.onload = () => callback();
-  }
+    const virtualImage = new Image();
+    virtualImage.src = src; // it makes load texture concurrently using disc cache
+    if (callback) {
+        virtualImage.onload = () => callback();
+    }
 };
 
 type Debounce = <T extends any[]>(
-  func: (...args: T) => void,
-  delay: number
+    func: (...args: T) => void,
+    delay: number
 ) => (...args: T) => void;
 export const debounce: Debounce = (func, delay) => {
-  let timeout: ReturnType<typeof setTimeout> | null = null;
-  return (...args) => {
-    if (timeout) {
-      clearTimeout(timeout);
-    }
-    timeout = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
+    let timeout: ReturnType<typeof setTimeout> | null = null;
+    return (...args) => {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(() => {
+            func(...args);
+        }, delay);
+    };
+};
+type Throttle = <T extends any[]>(
+    func: (...args: T) => void,
+    limit: number
+) => (...args: T) => void;
+
+export const throttle: Throttle = (func, limit) => {
+    let throttling = false;
+
+    return (...args) => {
+        if (!throttling) {
+            throttling = true;
+            func(...args);
+
+            setTimeout(() => {
+                throttling = false;
+            }, limit);
+        }
+    };
 };
